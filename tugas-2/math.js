45ff5202775f481c780ac9777c05eff1d11d0d3e
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-12-03 21:22:29 +0700

    math.js

---
+++ tugas-2/math.js
@@ -0,0 +1,9 @@
+function luasLingkaran(r) {
+  return Math.PI * r * r;
+}
+console.log(luasLingkaran(14));
+
+function arrayAngka(numbers) {
+  return numbers.map((num) => num ** 2);
+}
+console.log(arrayAngka([1, 2, 3, 4, 5]));
